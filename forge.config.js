a44e7194b717d14e19dde0f0eb82e770df86e49a
Derek Dardzinski <dardzinski.derek@gmail.com>
2023-10-23 19:59:07 -0700

    Adding publish action

--- forge.config.js
+++ forge.config.js
@@ -24,6 +24,18 @@ module.exports = {
     //   config: {},
     // },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'DerekDardzinski',
+          name: 'OgreInterface-eforge'
+        },
+        prerelease: true
+      }
+    }
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
